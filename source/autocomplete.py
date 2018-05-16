bafa1904665160fda7938d9d1eb7c075940c6a20
pomfy113 <pomfy113@gmail.com>
2018-05-15 20:58:57 -0700

    Working so far. Now to optimizing.

--- source/autocomplete.py
+++ source/autocomplete.py
@@ -7,7 +7,7 @@ class TrieNode:
 
     # Trie node class
     def __init__(self):
-        self.children = [None]*27
+        self.children = {}
         self.end = False
 
 class Trie:
@@ -18,51 +18,44 @@ class Trie:
     def getNode(self):
         return TrieNode()
 
-    def _charToIndex(self,ch):
-        if(ch == '-'):
-            return 26
-        return ord(ch) - 97
-
-
-    def insert(self,key):
+    def insert(self, word):
         node = self.root
-        length = len(key)
-        for level in range(length):
-            index = self._charToIndex(key[level])
+        for char in word:
+            if char not in node.children:
+                node.children[char] = self.getNode()
+            node = node.children[char]
 
-            # if current character is not present
-            if not node.children[index]:
-                node.children[index] = self.getNode()
-            node = node.children[index]
-
-        # mark last node as leaf
         node.end = True
 
-    def search(self, key):
+    def search(self, prefix):
         node = self.root
-        length = len(key)
-        for level in range(length):
-            index = self._charToIndex(key[level])
-            if not node.children[index]:
+        wordlist = []
+
+        for char in prefix:
+            if char not in node.children:
                 return False
+            node = node.children[char]
+        if node.end:
+            wordlist.append(prefix)
 
-            node = node.children[index]
+        return self.traverse(node, prefix, wordlist)
 
-        return self.traverse(node, key)
-
-    def traverse(self, node, word, wordlist=[]):
+    def traverse(self, node, word, wordlist):
+        # If it has children, keeep going
         if any(node.children):
-            for index, child in enumerate(node.children):
-                if child:
-                    newword = word + chr(index + 97)
-                    if child.end:
-                        wordlist.append(newword)
-                    self.traverse(child, newword, wordlist)
+            # For every item in the children
+            for key in node.children:
+                child = node.children[key]
+                newword = word + key
+                # If it's a proper word
+                if child.end:
+                    wordlist.append(newword)
+                # Keep going!
+                self.traverse(child, newword, wordlist)
             return wordlist
+        # Else, just return the word list
         else:
-            if node.end:
-                return wordlist
-        # return True
+            return wordlist
 
 def get_words(filename):
     list = []
@@ -73,22 +66,29 @@ def get_words(filename):
     return inputfile.split('\n')[:-1]
 
 def autocomplete(prefix):
-    if os.path.exists('auto.p'):
-        trie = pickle.load( open( "auto.p", "rb" ) )
-    else:
-        words = get_words('/usr/share/dict/words')
-        # words = get_words('./dictionarytest.txt')
-        trie = Trie()
+    start_time = time.time()
 
-        for word in words:
-            trie.insert(word.lower())
-
-        pickle.dump(trie, open( "auto.p", "wb" ))
-
+    # if os.path.exists('auto.p'):
+    #     trie = pickle.load( open( "auto.p", "rb" ) )
+    #     all_words = pickle.load( open("word.p", "rb") )
+    # else:
     all_words = get_words('/usr/share/dict/words')
+    # all_words = get_words('./dictionarytest.txt')
+    trie = Trie()
+
+    for word in all_words:
+        trie.insert(word.lower())
+
+    # print(trie.search(prefix))
+        # pickle.dump(trie, open( "auto.p", "wb" ))
+        # pickle.dump(all_words, open( "word.p", "wb") )
+
     all_prefixes = set([word[:len(word)//2] for word in all_words])
-    time = benchmark(trie, all_prefixes)
-    print('Took {} seconds to benchmark {} prefixes on {} words'.format(time, len(all_prefixes), len(all_words)))
+ #
+    print("Dictionary build time: {} seconds".format(time.time() - start_time))
+
+    bench_time = benchmark(trie, all_prefixes)
+    print('Took {} seconds to benchmark {} prefixes on {} words'.format(bench_time, len(all_prefixes), len(all_words)))
 
 
 
@@ -104,6 +104,7 @@ def main():
     """Perform main function."""
     start_time = time.time()
     autocomplete(sys.argv[1])
+    print("Total time, counting dict build: {} seconds".format(time.time() - start_time))
 
     return
 
